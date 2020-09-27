6801662276a1b30b6c081d9d6ed4361deeb6abd5
WebGuru <hugo.castelo.pires@gmail.com>
2020-09-27 21:12:49 +0100

    Add files via upload

---
+++ Webguru/TotalsAtCheckout/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+	\Magento\Framework\Component\ComponentRegistrar::MODULE, 
+	'Webguru_TotalsAtCheckout', 
+	__DIR__
+);
---
+++ Webguru/TotalsAtCheckout/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Checkout/js/view/summary/abstract-total': {
+                'Webguru_TotalsAtCheckout/js/abstract-total-mixin': true
+            }
+        }
+    }
+};
